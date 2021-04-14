f69146720de7451f1fedc83e76a07554a013ebd3
Wolfgang Wagner <wagner@wowa-webdesign.de>
2021-04-14 13:07:09 +0200

    [TASK] Simpler JS for scrolling

--- Resources/Public/JavaScript/custom.js
+++ Resources/Public/JavaScript/custom.js
@@ -2,20 +2,13 @@
 var headerHeight = $("#mainmenu").outerHeight();
 var docTitle = document.title;
 
-$('#mainmenu').on('click touchend', 'a[href*="/#"]', function(event) {
-    //event.preventDefault();
-    var $this = $(this);
-
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-        location.hostname == this.hostname) {
-
-        var $target = $(this.hash);
-        $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
-        if ($target.length) {
-            var targetOffset = $target.offset().top - headerHeight;
-            $('html,body').stop().animate({ scrollTop: targetOffset }, 1000);
-            $this.parent().addClass('is-active').siblings().removeClass('is-active');
-        }
-    }
-    return false;
+$('a[href^="#"]').on('click',function(e) {
+    e.preventDefault();
+    var target = this.hash;
+    var $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
 });
