480b6478147112d1456d780dc38d98d7782b70d8
Wolfgang Wagner <wagner@wowa-webdesign.de>
2021-04-14 18:35:57 +0200

    [TASK] Add fields for css id and class to page properties

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,36 @@
+<?php
+
+$newPageColumns = array(
+    'ww_css_id' => array(
+        'exclude' => 0,
+        'label' => 'LLL:EXT:wwonepagetemplate/Resources/Private/Language/locallang.xlf:ww_css_id',
+        'config' => array(
+            'type' => 'input',
+            'size' => 30,
+            'eval' => 'nospace,alphanum_x,lower,unique,required'
+        ),
+    ),
+    'ww_css_class' => array(
+        'exclude' => 0,
+        'label' => 'LLL:EXT:wwonepagetemplate/Resources/Private/Language/locallang.xlf:ww_css_class',
+        'config' => array(
+            'type' => 'input',
+            'size' => 30,
+            'eval' => 'nospace,alphanum_x,lower,unique'
+        ),
+    ),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $newPageColumns);
+
+$GLOBALS['TCA']['pages']['palettes']['wwonepagetemplate'] = array(
+    'canNotCollapse' => 1,
+    'showitem' => 'ww_css_id, ww_css_class'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'pages',
+    '--palette--;LLL:EXT:wwonepagetemplate/Resources/Private/Language/locallang.xlf:palettename;wwonepagetemplate,',
+    '',
+    'after:layout'
+);
---
+++ ext_tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE pages (
+	ww_css_class varchar(255) DEFAULT '' NOT NULL,
+	ww_css_id    varchar(255) DEFAULT '' NOT NULL,
+);
