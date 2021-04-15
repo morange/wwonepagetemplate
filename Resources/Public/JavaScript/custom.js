89af9e897b6e173e3dda6aa37f8c97d44cbbc51b
Wolfgang Wagner <wagner@wowa-webdesign.de>
2021-04-15 13:53:00 +0200

    [TASK] Mark active links in main navigation

--- Resources/Public/JavaScript/custom.js
+++ Resources/Public/JavaScript/custom.js
@@ -2,13 +2,18 @@
 var headerHeight = $("#mainmenu").outerHeight();
 var docTitle = document.title;
 
-$('a[href^="/#"]').on('click',function(e) {
+$('a[href^="/#"]').on('click', function (e) {
     e.preventDefault();
     var target = this.hash;
     var $target = $(target);
+    $(this).parent().addClass('is-active').siblings().removeClass('is-active');
     $('html, body').stop().animate({
         'scrollTop': $target.offset().top
     }, 900, 'swing', function () {
         window.location.hash = target;
     });
+});
+$('a.navbar-brand').on('click', function (e) {
+    e.preventDefault();
+    $('ul.navbar-nav li').removeClass('is-active');
 });
