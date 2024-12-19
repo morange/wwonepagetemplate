5e728458355df2a201f6b6ce42ae653438a391de
Steffen Matthes <git@matthes-hofer.de>
2024-12-19 18:04:55 +0100

    [TASK] Set version compatibility to TYPO3 v13 delete old files

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('TYPO3')) {
-    die('Access denied.');
-}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('wwonepagetemplate', 'Configuration/TypoScript', 'Onepage Template');
--- ext_localconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('TYPO3')) {
-    die('Access denied.');
-}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('@import "EXT:wwonepagetemplate/Configuration/TSconfig/User.tsconfig"');
